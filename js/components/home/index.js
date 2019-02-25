e64d01316183cf390d7148c2dc918215a9d0cea6
DURANDEAU <alexandredurandeau@gmail.com>
2019-02-25 18:51:41 +0100

    ADD HomePage + Btn Route Config

--- js/components/home/index.js
+++ js/components/home/index.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { Redirect } from 'react-router-dom'
 
 class Home extends React.Component {
+
+    state = {
+        redirect: false
+    }
+    setRedirect = () => {
+        this.setState({
+            redirect: true
+        })
+    }
+    renderRedirect = () => {
+        if (this.state.redirect) {
+            return <Redirect to='/configuration' />
+        }
+    }
+
     render() {
         return (
-            <div>
-                Bonsoir
-           </div>
+            <div className="body-home">
+                {this.renderRedirect()}
+                <button className="btnToConfig" onClick={this.setRedirect}>Passez à la configuration</button>
+            </div>
         );
     }
 }
